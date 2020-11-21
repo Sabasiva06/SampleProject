ed5b70de72d9ffd6a8c9da73908127adf7327365
Sabasiva06 <sabasiva06@gmail.com>
2020-11-21 11:21:28 +0530

    siva works

--- src/test/java/org/test/TestRunner.java
+++ src/test/java/org/test/TestRunner.java
@@ -6,9 +6,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"src\\test\\resources"}, 
-glue= {"org.step"})
+@CucumberOptions(features = { "src\\test\\resources" }, glue = { "org.step" })
 
 public class TestRunner {
+	public static void main(String[] args) {
+		System.out.println("my workk");
+		System.out.println("dknkdvndskkvns");
+
+	}
 
 }
